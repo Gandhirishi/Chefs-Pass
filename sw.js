589627f27613a085384644a84f60b502779b2fb1
Gandhirishi <19944255+Gandhirishi@users.noreply.github.com>
2026-09-23 16:57:18 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const ASSETS_TO_CACHE = [
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './screenshots/screenshot-mobile.png',
+  './screenshots/screenshot-desktop.png'
 ];
 
 self.addEventListener('install', (event) => {
